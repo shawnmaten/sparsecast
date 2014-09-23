05ef969c50d6e1ea8a8e98f979e9128387524cf7
shawnaten <shawnmaten@gmail.com>
2014-09-23 16:51:41 -0500

    Created placeholder for Constants.java in case I want to share this in the future.

---
+++ backend/src/main/java/com/shawnaten/simpleweather/backend/EmptyConstants.java
@@ -0,0 +1,16 @@
+package com.shawnaten.simpleweather.backend;
+
+/**
+ * Created by Shawn Aten on 8/4/14.
+ */
+public class EmptyConstants {
+
+    public static final String ANDROID_RELEASE_ID = "";
+    public static final String ANDROID_DEBUG_ID = "";
+    public static final String WEB_APP_ENGINE_ID = "";
+    public static final String WEB_LOCAL_ID = "";
+
+    public static final String PUBLIC_GOOGLE_API_KEY = "";
+    public static final String PUBLIC_FORECAST_API_KEY = "";
+
+}
