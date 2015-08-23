097a51aed87c7472cde77d69039bc39aa0539921
shawnaten <shawnmaten@gmail.com>
2015-08-23 14:11:19 -0500

    updating

--- app/src/main/java/com/shawnaten/simpleweather/tools/GeneralAlertDialog.java
+++ app/src/main/java/com/shawnaten/simpleweather/tools/GeneralAlertDialog.java
@@ -3,18 +3,16 @@ package com.shawnaten.simpleweather.tools;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 
-import com.shawnaten.simpleweather.R;
-
 public class GeneralAlertDialog extends DialogFragment {
-    private static final String TAG = "tag", TITLE = "title", MESSAGE = "message",
-            NEGATIVE_BUTTON = "negative", POSITIVE_BUTTON = "positive";
-
-    public GeneralAlertDialog() {
-
-    }
+    private static final String TAG = "tag";
+    private static final String TITLE = "title";
+    private static final String MESSAGE = "message";
+    private static final String NEGATIVE_BUTTON = "negative";
+    private static final String POSITIVE_BUTTON = "positive";
 
     public static GeneralAlertDialog newInstance(String tag, String title, String message,
         String negativeString, String positiveString) {
@@ -32,36 +30,26 @@ public class GeneralAlertDialog extends DialogFragment {
         return dialog;
     }
 
-	@Override
+	@NonNull
+    @Override
     public Dialog onCreateDialog(final Bundle savedInstanceState) {
-        Dialog dialog;
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(), R.style.Theme_AppCompat_Light_Dialog_Alert);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         Bundle args = getArguments();
+
         builder.setTitle(args.getString(TITLE));
         builder.setMessage(args.getString(MESSAGE));
-        if (args.containsKey(NEGATIVE_BUTTON)) {
-            builder.setNegativeButton(args.getString(NEGATIVE_BUTTON), new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-
-                }
-            });
-        }
-        if (args.containsKey(POSITIVE_BUTTON)) {
-            builder.setPositiveButton(args.getString(POSITIVE_BUTTON), new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-
-                }
-            });
-        }
-        dialog =  builder.create();
-        //dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
-        return dialog;
+        if (args.containsKey(NEGATIVE_BUTTON))
+            builder.setNegativeButton(args.getString(NEGATIVE_BUTTON), new MyOnClickListener());
+        if (args.containsKey(POSITIVE_BUTTON))
+            builder.setNegativeButton(args.getString(POSITIVE_BUTTON), new MyOnClickListener());
+        return builder.create();
     }
 
-    public interface OnClickListener {
-        void onDialogClick(String tag, Boolean positive);
+    private class MyOnClickListener implements DialogInterface.OnClickListener {
+
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+
+        }
     }
-	
 }
