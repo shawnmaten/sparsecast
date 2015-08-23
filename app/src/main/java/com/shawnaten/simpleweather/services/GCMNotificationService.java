b04d5907476b794c03d98b9086a75161df75ae44
shawnaten <shawnmaten@gmail.com>
2015-08-23 16:56:16 -0500

    open app when clicking notification

--- app/src/main/java/com/shawnaten/simpleweather/services/GCMNotificationService.java
+++ app/src/main/java/com/shawnaten/simpleweather/services/GCMNotificationService.java
@@ -1,16 +1,20 @@
 package com.shawnaten.simpleweather.services;
 
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 
 import com.google.android.gms.gcm.GcmListenerService;
 import com.shawnaten.simpleweather.R;
 import com.shawnaten.simpleweather.lib.model.MessagingCodes;
 import com.shawnaten.simpleweather.tools.ForecastIconSelector;
+import com.shawnaten.simpleweather.ui.MainActivity;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -56,11 +60,21 @@ public class GCMNotificationService extends GcmListenerService {
         if (icon == null || content ==  null)
             return;
 
+        Intent settingsIntent = new Intent(this, MainActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(MainActivity.class);
+        stackBuilder.addNextIntent(settingsIntent);
+
+        PendingIntent pendingIntent =
+                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
         NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext())
                 .setSmallIcon(ForecastIconSelector.getNotifyIcon(icon))
                 .setContentTitle(getString(R.string.app_name))
                 .setContentText(content)
+                .setContentIntent(pendingIntent)
                 .setSound(soundUri)
+                .setAutoCancel(true)
                 .setPriority(NotificationCompat.PRIORITY_MAX);
 
         notifyManager.notify(getID(), builder.build());
