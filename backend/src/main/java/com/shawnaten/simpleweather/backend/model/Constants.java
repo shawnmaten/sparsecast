ce7375729b8b5f9dec1782c4864f8163417216c2
Shawn Aten <shawnmaten@gmail.com>
2017-04-30 15:48:30 -0500

    Remove unused API key variables.
    These were already moved and used from another file.

--- backend/src/main/java/com/shawnaten/simpleweather/backend/model/Constants.java
+++ backend/src/main/java/com/shawnaten/simpleweather/backend/model/Constants.java
@@ -10,8 +10,4 @@ public class Constants {
     public static final String WEB_APP_ENGINE_ID = "332711553534-gm4cv0ehp7arasfkqvfegmplr4t0hhgl.apps.googleusercontent.com";
     public static final String WEB_LOCAL_ID = "332711553534-pa1b3kmulgvcsh3dbsc2j1um3nio0tt1.apps.googleusercontent.com";
 
-    public static final String PUBLIC_GOOGLE_API_KEY = "AIzaSyA9KQlQKoxlD6XWtl8kAs2eP4rmUtwxxnM";
-    public static final String PUBLIC_FORECAST_API_KEY = "92a3c52899fb76bf7c69d2edf946f191";
-    public static final String PUBLIC_INSTAGRAM_API_KEY = "03bb4eb2867d4ee38dc5ee9395e9833c";
-
 }
