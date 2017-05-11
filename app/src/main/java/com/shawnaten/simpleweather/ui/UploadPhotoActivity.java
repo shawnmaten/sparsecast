fd4f5a1697fdde48932a4396491c13ea9c11c0fe
Shawn Aten <shawnmaten@gmail.com>
2017-05-10 21:29:55 -0500

    Add activity for photo upload.

---
+++ app/src/main/java/com/shawnaten/simpleweather/ui/UploadPhotoActivity.java
@@ -0,0 +1,47 @@
+package com.shawnaten.simpleweather.ui;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.shawnaten.simpleweather.R;
+import com.shawnaten.simpleweather.tools.Attributions;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class UploadPhotoActivity extends BaseActivity {
+
+    @Bind(R.id.toolbar) Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_about);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+
+}
+
+
